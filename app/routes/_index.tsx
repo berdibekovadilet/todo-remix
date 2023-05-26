6ada5b91250a84e2dffddd63b7cb7e67777b962a
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-05-26 15:37:56 +0600

    add form and list of notes

---
+++ app/routes/_index.tsx
@@ -0,0 +1,27 @@
+import type {V2_MetaFunction} from "@remix-run/node";
+import {Link} from '@remix-run/react'
+import {LinksFunction} from "@remix-run/node";
+import homeStyles from "~/styles/home.css";
+
+export const meta: V2_MetaFunction = () => {
+    return [
+        {title: "New Remix App"},
+        {name: "description", content: "Welcome to Remix!"},
+    ];
+};
+
+export const links: LinksFunction = () => [{rel: "stylesheet", href: homeStyles}]
+
+export default function Index() {
+    return (
+        <main id='content'>
+            <h1>A better way of keeping track of your notes</h1>
+            <p>Try our early beta and never loose track of your notes again!</p>
+            <p id='cta'>
+                <Link to='/notes'>Try Now!</Link>
+            </p>
+        </main>
+    );
+}
+
+
